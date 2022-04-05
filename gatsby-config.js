997afe090eedcf78e81a06ce49c553dc882df0f8
Kirill Skok <52896622+kirillsqoq@users.noreply.github.com>
2022-04-05 04:03:16 +0300

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyAfxteKn2ks9t0VahryPrCDK9HUNDZ7nNQ",
+          authDomain: "cryptodonat-9b4c7.firebaseapp.com",
+          projectId: "cryptodonat-9b4c7",
+          storageBucket: "cryptodonat-9b4c7.appspot.com",
+          messagingSenderId: "740701682510",
+          appId: "1:740701682510:web:c5262c59bc5138b8e4fc1f",
+        },
+      },
+    },
+  ],
+}
